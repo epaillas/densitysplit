aae0c193d8796a88289ed98e54280aa24bc74d31
Enrique Paillas <epaillas@astro.puc.cl>
2023-02-26 21:44:01 -0500

    update

--- densitysplit/pipeline.py
+++ densitysplit/pipeline.py
@@ -5,10 +5,48 @@ from densitysplit import filters
 
 
 class DensitySplit:
+    """
+    Class to split a set of points into quantiles based on the local 
+    density field.
+
+    Parameters
+    ----------
+    data_positions : array_like
+        Positions of the data points.
+    boxsize : float, optional
+        Size of the box. If not provided, randoms are required.
+    data_weights : array_like, optional
+        Weights of the data points. If not provided, all points are 
+        assumed to have the same weight.
+    randoms_positions : array_like, optional
+        Positions of the random points. If not provided, boxsize must 
+        be provided.
+    randoms_weights : array_like, optional
+        Weights of the random points. If not provided, all points are 
+        assumed to have the same weight.
+
+    Attributes
+    ----------
+    data_positions : array_like
+        Positions of the data points.
+    boxsize : float
+        Size of the box.
+    data_weights : array_like
+        Weights of the data points.
+    randoms_positions : array_like
+        Positions of the random points.
+    randoms_weights : array_like
+        Weights of the random points.
+    mesh : CatalogMesh
+        CatalogMesh object.
+    density : array_like
+        Density field at the sampling positions.
+    sampling_positions : array_like
+        Positions where the density field was sampled.
+    """
     def __init__(self, data_positions, boxsize=None,
         data_weights=None, randoms_positions=None,
         randoms_weights=None):
-
         self.data_positions = data_positions
         self.boxsize = boxsize
 
@@ -28,6 +66,14 @@ class DensitySplit:
             self.randoms_positions = randoms_positions
 
     def get_mesh(self):
+        """
+        Get the CatalogMesh object.
+
+        Returns
+        -------
+        mesh : CatalogMesh
+            CatalogMesh object.
+        """
         if self.boxsize is None:
             mesh  = CatalogMesh(data_positions=self.data_positions,
                 data_weights=self.data_weights, cellsize=self.cellsize,
@@ -44,6 +90,21 @@ class DensitySplit:
 
 
     def get_box_randoms(self, nrandoms, seed=42):
+        """
+        Get random points inside the box.
+        
+        Parameters
+        ----------
+        nrandoms : int
+            Number of random points to generate.
+        seed : int, optional
+            Random seed.
+
+        Returns
+        -------
+        randoms : array_like
+            Random points inside the box.
+        """
         np.random.seed(seed)
         randoms = np.random.rand(nrandoms, 3) * self.boxsize
         return randoms
@@ -52,6 +113,36 @@ class DensitySplit:
     def get_density(self, smooth_radius, cellsize, compensate=True,
         resampler='cic', sampling='randoms', sampling_positions=None,
         boxpad=2.0, filter_shape='tophat'):
+        """
+        Get the density field.
+
+        Parameters
+        ----------
+        smooth_radius : float
+            Radius of the smoothing filter.
+        cellsize : float
+            Size of the cells in the mesh.
+        compensate : bool, optional
+            Compensate for the shot noise.
+        resampler : str, optional
+            Resampling method.
+        sampling : str, optional
+            Sampling method.
+        sampling_positions : array_like, optional
+            Positions where the density field should be sampled. If not 
+            provided, the randoms are used.
+        boxpad : float, optional
+            Padding of the box.
+        filter_shape : str, optional
+            Shape of the smoothing filter.
+
+        Returns
+        -------
+        density : array_like
+            Density field at the sampling positions.
+        sampling_positions : array_like
+            Positions where the density field was sampled.
+        """
         self.cellsize = cellsize
         self.boxpad = boxpad
         self.resampler = resampler
@@ -104,6 +195,23 @@ class DensitySplit:
         return self.density
 
     def get_quantiles(self, nquantiles, return_density=False):
+        """
+        Get the quantiles of the density field.
+
+        Parameters
+        ----------
+        nquantiles : int
+            Number of quantiles.
+        return_density : bool, optional
+            Return the density field at the quantiles.
+
+        Returns
+        -------
+        quantiles : array_like
+            Quantiles of the density field.
+        density_quantiles : array_like
+            Density field at the quantiles.
+        """
         quantiles_idx = qcut(self.density, nquantiles, labels=False)
         quantiles = []
         for i in range(nquantiles):
